0103440a4a02ee2b1b031fafd2b20eced62b2726
taidixiong233 <maohaoji2006@163.com>
2023-08-21 22:57:37 +0800

    完成了项目

---
+++ src/config.d.ts
@@ -0,0 +1,21 @@
+declare const config: {
+    port: number;
+    database: {
+        host: string;
+        port: number;
+        user: string;
+        password: string;
+        database: string;
+    };
+    secret: string;
+    user: Map<string, UserKey>;
+    timeout: number;
+};
+export interface UserKey {
+    ascii: string;
+    hex: string;
+    base32: string;
+    otpauth_url: string;
+    ip: string | string[];
+}
+export default config;
